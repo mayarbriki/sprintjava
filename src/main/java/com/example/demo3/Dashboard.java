7cd684a2b0bd23b6da7b29d68edfd25d9fa16dde
mayarbriki <100524582+mayarbriki@users.noreply.github.com>
2024-03-25 03:08:20 +0100

    dashboard

--- src/main/java/com/example/demo3/Dashboard.java
+++ src/main/java/com/example/demo3/Dashboard.java
@@ -1,4 +1,6 @@
 package com.example.demo3;
+import javafx.scene.control.TextArea;
+import java.sql.SQLException;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -7,8 +9,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+import services.ServiceProduit;
+
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
@@ -18,11 +27,26 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TextField;
 import models.Personne;
+import models.Produit;
 import services.ServicePersonne;
 import utils.MyDatabase;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
 
 
 public class Dashboard extends Application implements Initializable {
+    public String path;
+    @FXML
+    private Label imagePathLabel;
+
+    @FXML
+    private TextArea description;
+    @FXML
+    private AnchorPane anchorPane1;
+    @FXML
+    private TextField nomproduit;
+    @FXML
+    private AnchorPane anchorPane2;
     @FXML
     private Button add;
     @FXML
@@ -38,15 +62,20 @@ public class Dashboard extends Application implements Initializable {
     private Button commande;
     @FXML
     private ComboBox<String> comb;
-
+    @FXML
+    private Button addproduit;
     @FXML
     private Button livraison;
 
     @FXML
     private Button produit;
-
+    @FXML
+    private BorderPane layout;
     @FXML
     private Button user;
+    @FXML
+    private ImageView imgview;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
@@ -58,7 +87,7 @@ public class Dashboard extends Application implements Initializable {
     //void Select(ActionEvent event) {
       //  String role = comb.getSelectionModel().getSelectedItem().toString();
     //}
-    @FXML
+    /*@FXML
     private void handleProduitButtonClick(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("ajouterproduit.fxml"));
@@ -68,12 +97,36 @@ public class Dashboard extends Application implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }*/
+    @FXML
+    private void handleProduitButtonClick(ActionEvent event) {
+        // This method is already the event handler for the button click,
+        // so you don't need to define another event handler inside it.
+        // Simply put your logic directly here.
+
+        anchorPane1.setVisible(false);
+        anchorPane2.setVisible(true);
     }
+    @FXML
+    private void handleProduitButtonClick1(ActionEvent event) {
+        // This method is already the event handler for the button click,
+        // so you don't need to define another event handler inside it.
+        // Simply put your logic directly here.
+
+        anchorPane1.setVisible(true);
+        anchorPane2.setVisible(false);
+    }
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ObservableList<String> list = FXCollections.observableArrayList("admin", "livreur");
         comb.setItems(list);
+        anchorPane1.setVisible(true);
+
+        // Set anchorPane2 invisible initially
+        anchorPane2.setVisible(false);
     }
     public void add(ActionEvent event) throws SQLException {
         Connection connection = MyDatabase.getInstance().getConnection();
@@ -98,6 +151,59 @@ public class Dashboard extends Application implements Initializable {
             System.err.println(e.getMessage());
         }
     }
+    @FXML
+    Label label = new Label();
+    public void insertImage() {
+
+        // String chosenImagePath;
+        FileChooser open = new FileChooser();
+
+        Stage stage = (Stage) layout.getScene().getWindow();
+
+        File file = open.showOpenDialog(stage);
+
+        if (file != null) {
+
+            path = file.getAbsolutePath();
+
+            path = path.replace("\\", "\\\\");
+            imagePathLabel.setText(path);
+
+            label.setText(path);
+
+            Image image = new Image(file.toURI().toString(), 110, 110, false, true);
+
+            imgview.setImage(image);
+
+        } else {
+
+            System.out.println("NO DATA EXIST!");
+
+        }
+    }
+    public void addproduit1(ActionEvent event) throws SQLException {
+        Connection connection = MyDatabase.getInstance().getConnection();
+        if (connection == null) {
+            System.out.println("Connection is null!");
+            return;
+        }
+        //  String imagePath = insertImage();  // Call insertImage to get the path (or null)
+        String imagePath = path;
+        if (imagePath == null) {
+            System.out.println("No image selected!");
+            return;
+        }
+        Produit produit = new Produit(25, nomproduit.getText(), description.getText(), imagePath);
+        ServiceProduit sp = new ServiceProduit(); // Use connection object
+
+        try {
+            sp.ajouter(produit);
+            System.out.println(sp.recuperer());
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
 
 
 
